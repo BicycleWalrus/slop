2a1df50e9c07aafe76ffd4035d523be9aceb2ab6
BicycleWalrus <tpatricksc@gmail.com>
2022-03-02 21:08:59 +0000

    learning

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def main():
+    user_input = input("Please input an IPv4 IP address:")
+    print("You told me the IPv4 Address is: " + user_input)
+
+main()
---
+++ lab_input/iptaker02.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def main():
+    user_input = input("Please input an IPv4 IP Address: ")
+    print("You told me the IPv4 address is: ", user_input)
+
+    vendor = input("Please input the vendor name: ")
+    print(vendor)
+
+main()
